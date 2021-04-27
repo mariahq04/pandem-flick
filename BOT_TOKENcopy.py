a9875dd65e80afb576b06169cb675f8de3d595d8
zz68174 <78423389+zz68174@users.noreply.github.com>
2021-04-27 19:29:31 -0400

    Update BOT_TOKENcopy.py

--- BOT_TOKENcopy.py
+++ BOT_TOKENcopy.py
@@ -1,2 +1,6 @@
 # Put the discord generated token for a specific bot here
+<<<<<<< Updated upstream
 TOKEN = ''
+=======
+TOKEN = 'ODI4NzI2NjY4NTAzNjc5MDY3.YGtx2g.ZSZ5OWMRp7UJjUHSUgmKrUzKsmM'
+>>>>>>> Stashed changes
